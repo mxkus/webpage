c2d37034b45205b3aa88c12f4e141bc16358573a
Maximilian Kusterer <m.kusterer@enbw.com>
2021-01-22 12:25:23 +0100

    format predictions float

--- js/main.js
+++ js/main.js
@@ -317,7 +317,7 @@ var main = (function () {
                 const model = await mobilenet.load();
                 const predictions = await model.classify(img);
                 console.log(predictions);
-                const result = predictions.map(pred => pred.className.split(",")[0] + ": "+ pred.probability).join("\n");
+                const result = predictions.map(pred => pred.className.split(",")[0] + ": "+ pred.probability.substring(0, 5)).join("\n");
                 document.body.removeChild(image);
                 this.type(result, this.unlock.bind(this))
                 break;
